3cf96bacc62768a98a43d2eff5fc5dc830460be4
MehruzSaif <mehruzsaif007@gmail.com>
2022-03-26 03:11:53 +0600

    keyboard.js & keyboard.css file updated

--- src/component/Keyboard/Keyboard.js
+++ src/component/Keyboard/Keyboard.js
@@ -7,7 +7,15 @@ const Keyboard = (props) => {
     return (
         <div className='keyboard'>
             <img src={img} alt="" />
-            <p className='keyboard-name'>{name}</p>
+
+            <div className='keyboard-info'>
+                <p className='keyboard-name'>{name}</p>
+                <p className='keyboard-price'>Price: ${price}</p>
+            </div>
+
+            <button className='btn-cart'>
+                <p>Add To Cart</p>
+            </button>
         </div>
     );
 };
