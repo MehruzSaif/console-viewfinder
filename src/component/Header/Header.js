be8699b1213b80d92551cbebed14a328f3704c9a
MehruzSaif <mehruzsaif007@gmail.com>
2022-03-25 23:29:23 +0600

    added Header component

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Keyboard Finder</h1>
+            <h2>Choose your 4 consoles</h2>
+        </div>
+    );
+};
+
+export default Header;
